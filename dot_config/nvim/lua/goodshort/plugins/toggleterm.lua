37743e52a869d53feeb3dd02ced70292f2b48bc7
Adrien Biencourt <adrien@revanista.com>
2023-02-09 16:50:25 +0000

    Renamed function properly

--- dot_config/nvim/lua/goodshort/plugins/toggleterm.lua
+++ dot_config/nvim/lua/goodshort/plugins/toggleterm.lua
@@ -41,7 +41,7 @@ return function()
     local Terminal = require("toggleterm.terminal").Terminal
     local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
-    function _lazygit_toggle()
+    function _LAZYGIT_TOGGLE()
         lazygit:toggle()
     end
 
