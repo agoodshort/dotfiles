d3aad559e38157bbec19e523ba337da771ab8955
Adrien Biencourt <adrien@revanista.com>
2023-02-12 19:36:22 +0000

    Cleanup terminal functions

--- dot_config/nvim/lua/goodshort/plugins/toggleterm.lua
+++ dot_config/nvim/lua/goodshort/plugins/toggleterm.lua
@@ -39,27 +39,9 @@ return function()
     vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
     local Terminal = require("toggleterm.terminal").Terminal
-    local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+    local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
 
     function _LAZYGIT_TOGGLE()
         lazygit:toggle()
-    end
-
-    local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
-
-    function _NCDU_TOGGLE()
-        ncdu:toggle()
-    end
-
-    local htop = Terminal:new({ cmd = "htop", hidden = true })
-
-    function _HTOP_TOGGLE()
-        htop:toggle()
-    end
-
-    local python = Terminal:new({ cmd = "python", hidden = true })
-
-    function _PYTHON_TOGGLE()
-        python:toggle()
     end
 end
