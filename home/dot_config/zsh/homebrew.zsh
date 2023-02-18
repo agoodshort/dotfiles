dbc874023940d87643d1e0c54c0ac8bcb37d1372
Adrien Biencourt <adrien@revanista.com>
2023-02-18 13:32:50 +0000

    zlua added throu homebrew

--- home/dot_config/zsh/homebrew.zsh
+++ home/dot_config/zsh/homebrew.zsh
@@ -8,3 +8,7 @@ export "_Z_DATA=$XDG_CONFIG_HOME/z"
 
 # zsh-autosuggestions
 [[ -f "$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]] && source "$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+
+# skywind3000/z.lua
+[[ -f "$HOMEBREW_PREFIX/share/z.lua/z.lua" ]] && eval "$(lua $HOMEBREW_PREFIX/share/z.lua/z.lua --init zsh)"
+export "_ZL_DATA=$XDG_CONFIG_HOME/zlua"
