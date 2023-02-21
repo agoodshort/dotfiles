adf09efc02739f68c3cc0dabc66e9df8278a15aa
Adrien Biencourt <adrien@revanista.com>
2023-02-21 16:24:41 +0000

    Nothing special

--- home/dot_config/nvim/lua/goodshort/plugins/toggleterm.lua
+++ home/dot_config/nvim/lua/goodshort/plugins/toggleterm.lua
@@ -6,7 +6,7 @@ return function()
 
     toggleterm.setup({
         size = 20,
-        open_mapping = [[<c-\>]],
+        open_mapping = [[<C-\>]],
         hide_numbers = true,
         shade_filetypes = {},
         shade_terminals = true,
