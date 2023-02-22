03545c620f244a1a1ba0cfcd25263e7152a72e29
Adrien Biencourt <adrien@revanista.com>
2023-02-22 02:53:31 +0000

    COnfiguration continues

--- home/dot_config/starship/executable_chezmoi_status_diff.sh
+++ home/dot_config/starship/executable_chezmoi_status_diff.sh
@@ -12,8 +12,14 @@ destinationStatus=$(echo "$status" | cut -c 1)
 # source
 [[ $(echo $sourceStatus | wc -l) > 0 ]] && output="${output}⇣"
 
+# destination
+[[ $(echo $destinationStatus | wc -l) > 0 ]] && output="${output}⇡"
+
 # modified
 [[ ($sourceStatus == *'M'*) || ($destinationStatus == *'M'*) ]] && output="${output}!"
+
+# added
+[[ ($sourceStatus == *'A'*) || ($destinationStatus == *'A'*) ]] && output="${output}+"
 
 # deleted
 [[ ($sourceStatus == *'D'*) || ($destinationStatus == *'D'*) ]] && output="${output}✘"
