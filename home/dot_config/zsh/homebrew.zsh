7f84680d06f90550e1bddbc5c7c5a0061dc6d8ac
Adrien Biencourt <adrien@revanista.com>
2023-02-23 16:09:07 +0000

    homebrew file cleanup

--- home/dot_config/zsh/homebrew.zsh
+++ home/dot_config/zsh/homebrew.zsh
@@ -1,18 +1,55 @@
-## Installed through Homebrew
+## Homebrew config
+export "HOMEBREW_NO_ENV_HINTS=1"
+
 # zsh-syntax-highlighting
 [[ -f "$HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]] && source "$HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 
 # zsh-autosuggestions
 [[ -f "$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]] && source "$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
-# ajeetdsouza/zoxide
-export "_ZO_DATA_DIR=$XDG_CONFIG_HOME/zoxide"
-[[ -f "$HOMEBREW_PREFIX/bin/zoxide" ]] && eval "$(zoxide init zsh)"
-
-# junegunn/fzf
-# Required to use `zf` with zoxide
+## ajeetdsouza/zoxide
+if [[ -f "$HOMEBREW_PREFIX/bin/zoxide" ]]; then
+    export "_ZO_DATA_DIR=$XDG_CONFIG_HOME/zoxide"
+    eval "$(zoxide init zsh)"
+fi
+# junegunn/fzf - Required to use `zf` with zoxide (might be used by some other plugins)
 [[ -f $XDG_CONFIG_HOME/fzf/fzf.zsh ]] && source $XDG_CONFIG_HOME/fzf/fzf.zsh
 
+# awscli
+if [[ -f "$HOMEBREW_PREFIX/bin/aws" ]]; then
+    export AWS_PROFILE=revanistaLocal
+    export AWS_REGION=eu-west-1
+fi
+
 # starship
-export "STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/starship.toml"
-[[ -x $(which starship) ]] && eval "$(starship init zsh)"
+if [[ -f "$HOMEBREW_PREFIX/bin/starship" ]]; then
+    eval "$(starship init zsh)"
+    export "STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/starship.toml"
+fi
+
+## nvm
+# lazy loaded manually
+if [[ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" && -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ]]; then
+    lazynvm() {
+        unset -f nvm node npm
+        export NVM_DIR="$XDG_CONFIG_HOME/nvm"
+        mkdir -p $NVM_DIR
+        source "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"  # This loads nvm
+        source "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+    }
+
+    nvm() {
+        lazynvm
+        nvm $@
+    }
+
+    node() {
+        lazynvm
+        nvm $@
+    }
+
+    npm() {
+        lazynvm
+        nvm $@
+    }
+fi
