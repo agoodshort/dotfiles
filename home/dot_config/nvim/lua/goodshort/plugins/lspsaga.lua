66bc263dfaeb69aedcfc6e55a5c9b06cc52d203c
Adrien Biencourt <adrien@revanista.com>
2023-02-27 08:38:33 +0000

    Remoed lightbulb sign on the left

--- home/dot_config/nvim/lua/goodshort/plugins/lspsaga.lua
+++ home/dot_config/nvim/lua/goodshort/plugins/lspsaga.lua
@@ -50,7 +50,7 @@ return function()
         lightbulb = {
             enable = true,
             enable_in_insert = true,
-            sign = true,
+            sign = false,
             sign_priority = 40,
             virtual_text = true,
         },
