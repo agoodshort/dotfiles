ad6cc1016eb7d685ad5d9226dc5398075091c52b
Adrien Biencourt <adrien@revanista.com>
2023-03-08 11:33:57 +0000

    Added echo commands and brew autoremove

--- home/dot_config/zsh/function.zsh
+++ home/dot_config/zsh/function.zsh
@@ -2,13 +2,21 @@ whichbrew=$(which brew)
 
 function brew() {
     if [[ $1 == "install" ]]; then
+        echo "----- start: brew install -----"
         $whichbrew "$@"
+        echo "----- end: brew install -----"
+
+        echo "----- start: brew autoremove -----"
+        $whichbrew autoremove
+        echo "----- end: brew autoremove -----"
+
+        echo "----- start: Generate Brewfile -----"
         if [[ -n $HOMEBREW_BUNDLE_FILE ]]; then
-            echo "Generating Brewfile..."
             $whichbrew bundle dump --force --describe
         else
             echo "Impossible to generate Brewfile, HOMEBREW_BUNDLE_FILE isn't set"
         fi
+        echo "----- end: Generate Brewfile -----"
     else
         $whichbrew "$@"
     fi
