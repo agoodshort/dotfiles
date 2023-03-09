5ef0334f3395bba0ab1fa965dcfad8954ed3b677
Adrien Biencourt <adrien@revanista.com>
2023-03-09 23:25:04 +0000

    Edited layout

--- home/dot_config/nvim/exact_lua/goodshort/plugins/lualine.lua
+++ home/dot_config/nvim/exact_lua/goodshort/plugins/lualine.lua
@@ -1,59 +1,59 @@
 return function()
-    local status_lualine, lualine = pcall(require, "lualine")
-    if not status_lualine then
-        return
-    end
+	local status_lualine, lualine = pcall(require, "lualine")
+	if not status_lualine then
+		return
+	end
 
-    local config = {
-        options = {
-            theme = 'auto',
-            disabled_filetypes = { 'packer', 'neo-tree' }
-        },
-        sections = {
-            lualine_a = { 'mode' },
-            lualine_b = { 'branch', 'diff', 'diagnostics' },
-            lualine_c = { 'filename' },
-            lualine_x = { 'filetype' },
-            lualine_y = {},
-            lualine_z = { 'location' }
-        },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = { 'filename' },
-            lualine_x = { 'location' },
-            lualine_y = {},
-            lualine_z = {}
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {}
-    }
+	local config = {
+		options = {
+			theme = "auto",
+			disabled_filetypes = { "packer", "neo-tree" },
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "filename" },
+			lualine_c = { "branch", "diff" },
+			lualine_x = { "diagnostics" },
+			lualine_y = { "filetype" },
+			lualine_z = { "location" },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = {},
+			lualine_z = {},
+		},
+		tabline = {},
+		winbar = {},
+		inactive_winbar = {},
+		extensions = {},
+	}
 
-    local function ins_left(component)
-        table.insert(config.sections.lualine_y, component)
-    end
+	local function ins_left(component)
+		table.insert(config.sections.lualine_x, component)
+	end
 
-    ins_left {
-        -- Lsp server name .
-        function()
-            local msg = 'No Active Lsp'
-            local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-            local clients = vim.lsp.get_active_clients()
-            if next(clients) == nil then
-                return msg
-            end
-            for _, client in ipairs(clients) do
-                local filetypes = client.config.filetypes
-                if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                    return client.name
-                end
-            end
-            return msg
-        end,
-        icon = ' LSP:',
-    }
+	ins_left({
+		-- Lsp server name
+		function()
+			local msg = "No Active Lsp"
+			local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+			local clients = vim.lsp.get_active_clients()
+			if next(clients) == nil then
+				return msg
+			end
+			for _, client in ipairs(clients) do
+				local filetypes = client.config.filetypes
+				if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+					return client.name
+				end
+			end
+			return msg
+		end,
+		icon = " LSP:",
+	})
 
-    lualine.setup(config)
+	lualine.setup(config)
 end
