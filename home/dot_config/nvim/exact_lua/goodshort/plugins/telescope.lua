522ef5474272f75f87c6a60b24eba543e6192eb0
Adrien Biencourt <adrien@revanista.com>
2023-04-12 22:09:51 +0100

    Edited shortcut for split view from telescope

--- home/dot_config/nvim/exact_lua/goodshort/plugins/telescope.lua
+++ home/dot_config/nvim/exact_lua/goodshort/plugins/telescope.lua
@@ -15,6 +15,7 @@ return function()
 				i = {
 					["<C-k>"] = ta.move_selection_previous,
 					["<C-j>"] = ta.move_selection_next,
+					["<C-s>"] = ta.select_vertical,
 				},
 			},
 		},
@@ -27,14 +28,7 @@ return function()
 				entry_format = "state #$ID, $STAT, $TIME",
 				mappings = {
 					i = {
-						-- IMPORTANT: Note that telescope-undo must be available when telescope is configured if
-						-- you want to replicate these defaults and use the following actions. This means
-						-- installing as a dependency of telescope in it's `requirements` and loading this
-						-- extension from there instead of having the separate plugin definition as outlined
-						-- above.
-						["<cr>"] = require("telescope-undo.actions").yank_additions,
-						["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
-						["<C-cr>"] = require("telescope-undo.actions").restore,
+						["<CR>"] = require("telescope-undo.actions").restore,
 					},
 				},
 			},
