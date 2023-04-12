94c85456b1c70b4e9e8facfa1cc31cb97e9799b8
Adrien Biencourt <adrien@revanista.com>
2023-04-12 15:54:42 +0100

    Moved keymap to which-key

--- home/dot_config/nvim/exact_lua/goodshort/plugins/tabby.lua
+++ home/dot_config/nvim/exact_lua/goodshort/plugins/tabby.lua
@@ -1,9 +1,12 @@
 return function()
-    local status, tabline = pcall(require, "tabby.tabline")
-    if not status then
-        return
-    end
+	local status, tabline = pcall(require, "tabby.tabline")
+	if not status then
+		return
+	end
 
-    tabline.use_preset('tab_only', { tab_name = { name_fallback = function() return "" end } })
-    vim.keymap.set("n", "<C-t>r", ":TabRename ")
+	tabline.use_preset("tab_only", { tab_name = {
+		name_fallback = function()
+			return ""
+		end,
+	} })
 end
