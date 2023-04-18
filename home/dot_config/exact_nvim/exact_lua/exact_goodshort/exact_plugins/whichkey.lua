9933d15e3948d519fcacd38afe822c6920d0a600
Adrien Biencourt <adrien@revanista.com>
2023-04-18 01:06:29 +0100

    No need to require

--- home/dot_config/exact_nvim/exact_lua/exact_goodshort/exact_plugins/whichkey.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_goodshort/exact_plugins/whichkey.lua
@@ -81,169 +81,134 @@ return {
 		}, leader2_opts_visual)
 
 		-- neo tree --
-		local status_neotree, _ = pcall(require, "neo-tree")
-		if status_neotree then
-			wk.register({
-				-- e = {
-				-- 	name = "Neo-tree",
-					e = { "<Cmd>Neotree toggle<CR>", "List Keymaps" },
-				-- },
-			}, leader_opts)
-		end
+		wk.register({
+			-- e = {
+			-- 	name = "Neo-tree",
+			e = { "<Cmd>Neotree toggle<CR>", "List Keymaps" },
+			-- },
+		}, leader_opts)
 
 		-- telescope --
-		local status_telescope, _ = pcall(require, "telescope")
-		if status_telescope then
-			wk.register({
-				f = {
-					name = "Telescope", -- optional group name
-					k = { "<Cmd>Telescope keymaps<CR>", "List Keymaps" },
-					f = { "<Cmd>Telescope find_files<CR>", "Find Files" },
-					b = { "<Cmd>Telescope buffers<CR>", "Buffers" },
-					h = { "<Cmd>Telescope help_tags<CR>", "Help Tags" },
-					s = { "<Cmd>Telescope grep_string<CR>", "Grep String" },
-					l = { "<Cmd>Telescope live_grep<CR>", "Live Grep" },
-					z = { "<Cmd>Telescope zoxide list<CR>", "List z" },
-					e = { "<Cmd>Telescope env<CR>", "List Environment Variables" },
-					p = { "<Cmd>Telescope packer<CR>", "List Packer Plugins" },
-					t = { "<Cmd>Telescope http list<CR>", "List HTTP Status Code" },
-					u = { "<Cmd>Telescope undo<CR>", "Visualize Undo Tree" },
-					c = { "<Cmd>Telescope neoclip<CR>", "List Clipboard" },
-					["?"] = { "<Cmd>Telescope find_pickers<CR>", "List Telescope Pickers" },
-				},
-			}, leader_opts)
+		wk.register({
+			f = {
+				name = "Telescope", -- optional group name
+				k = { "<Cmd>Telescope keymaps<CR>", "List Keymaps" },
+				f = { "<Cmd>Telescope find_files<CR>", "Find Files" },
+				b = { "<Cmd>Telescope buffers<CR>", "Buffers" },
+				h = { "<Cmd>Telescope help_tags<CR>", "Help Tags" },
+				s = { "<Cmd>Telescope grep_string<CR>", "Grep String" },
+				l = { "<Cmd>Telescope live_grep<CR>", "Live Grep" },
+				z = { "<Cmd>Telescope zoxide list<CR>", "List z" },
+				e = { "<Cmd>Telescope env<CR>", "List Environment Variables" },
+				p = { "<Cmd>Telescope packer<CR>", "List Packer Plugins" },
+				t = { "<Cmd>Telescope http list<CR>", "List HTTP Status Code" },
+				u = { "<Cmd>Telescope undo<CR>", "Visualize Undo Tree" },
+				c = { "<Cmd>Telescope neoclip<CR>", "List Clipboard" },
+				["?"] = { "<Cmd>Telescope find_pickers<CR>", "List Telescope Pickers" },
+			},
+		}, leader_opts)
 
-			wk.register({ ["<C-p>"] = { "<Cmd>Telescope keymaps<CR>", "List Keymaps" } }, blank_opts)
+		wk.register({ ["<C-p>"] = { "<Cmd>Telescope keymaps<CR>", "List Keymaps" } }, blank_opts)
 
-			-- fugitive --
-			wk.register({
-				g = {
-					name = "Git Tools", -- optional name
-					s = { "<Cmd>Git<CR>", "Fugitive Git Status" },
-					f = { "<Cmd>Telescope git_files<CR>", "Telescope Git Files" },
-					c = { "<Cmd>Telescope git_commits<CR>", "Telescope Git Commits" },
-					g = { "<Cmd>Telescope lazygit_toggleterm<CR>", "LazyGit" },
-					u = { "<Cmd>lua _GITUI_TOGGLE()<CR>", "GitUI" },
-					b = { "<Cmd>Gitsigns toggle_current_line_blame<CR>", "Gitsigns Blame" },
-					d = { "<Cmd>DiffviewOpen<CR>", "Diffview" },
-				},
-			}, leader_opts)
-		end
+		-- fugitive --
+		wk.register({
+			g = {
+				name = "Git Tools", -- optional name
+				s = { "<Cmd>Git<CR>", "Fugitive Git Status" },
+				f = { "<Cmd>Telescope git_files<CR>", "Telescope Git Files" },
+				c = { "<Cmd>Telescope git_commits<CR>", "Telescope Git Commits" },
+				g = { "<Cmd>Telescope lazygit_toggleterm<CR>", "LazyGit" },
+				u = { "<Cmd>lua _GITUI_TOGGLE()<CR>", "GitUI" },
+				b = { "<Cmd>Gitsigns toggle_current_line_blame<CR>", "Gitsigns Blame" },
+				d = { "<Cmd>DiffviewOpen<CR>", "Diffview" },
+			},
+		}, leader_opts)
 
 		-- Tab Management --
-		local status_tab, _ = pcall(require, "tabby")
-		if status_tab then
-			wk.register({
-				["<C-t>"] = {
-					name = "Tabs", -- optional name
-					["n"] = { "<Cmd>tabnew<CR>:Dashboard<CR>", "New Tab" },
-					["x"] = { "<Cmd>tabclose<CR>", "Close Tab" },
-					["l"] = { "<Cmd>tabn<CR>", "Go To Right Tab" },
-					["h"] = { "<Cmd>tabp<CR>", "Go To Left Tab" },
-				},
-			}, blank_opts)
-			vim.keymap.set("n", "<C-t>r", ":TabRename ") -- to review
-		end
+		wk.register({
+			["<C-t>"] = {
+				name = "Tabs", -- optional name
+				["n"] = { "<Cmd>tabnew<CR>:Dashboard<CR>", "New Tab" },
+				["x"] = { "<Cmd>tabclose<CR>", "Close Tab" },
+				["l"] = { "<Cmd>tabn<CR>", "Go To Right Tab" },
+				["h"] = { "<Cmd>tabp<CR>", "Go To Left Tab" },
+			},
+		}, blank_opts)
+		vim.keymap.set("n", "<C-t>r", ":TabRename ") -- to review
 
 		-- toggleterm --
-		local status_toggleterm, _ = pcall(require, "toggleterm")
-		if status_toggleterm then
-			wk.register({
-				["\\"] = { -- map to "\"
-					name = "ToggleTerm", -- optional name
-					["\\"] = { "<Cmd>ToggleTermToggleAll<CR>", "Toggle All Terminals" },
-					["1"] = { "<Cmd>1ToggleTerm<CR>", "Terminal 1" },
-					["2"] = { "<Cmd>2ToggleTerm<CR>", "Terminal 2" },
-					["3"] = { "<Cmd>3ToggleTerm<CR>", "Terminal 3" },
-				},
-			}, leader_opts)
-		end
+		wk.register({
+			["\\"] = { -- map to "\"
+				name = "ToggleTerm", -- optional name
+				["\\"] = { "<Cmd>ToggleTermToggleAll<CR>", "Toggle All Terminals" },
+				["1"] = { "<Cmd>1ToggleTerm<CR>", "Terminal 1" },
+				["2"] = { "<Cmd>2ToggleTerm<CR>", "Terminal 2" },
+				["3"] = { "<Cmd>3ToggleTerm<CR>", "Terminal 3" },
+			},
+		}, leader_opts)
 
 		-- Trouble keymap --
-		local status_trouble, _ = pcall(require, "trouble")
-		if status_trouble then
-			wk.register({
-				x = {
-					name = "Trouble", -- optional group name
-					x = { "<Cmd>TroubleToggle<CR>", "Toggle Trouble" },
-					w = { "<Cmd>TroubleToggle workspace_diagnostics<CR>", "Workspace Diagnostics" },
-					d = { "<Cmd>TroubleToggle document_diagnostics<CR>", "Document Diagnostics" },
-					q = { "<Cmd>TroubleToggle quickfix<CR>", "Quickfix" },
-				},
-			}, leader_opts)
-			wk.register({
-				gR = { "<Cmd>TroubleToggle lsp_references<CR>", "LSP References" },
-			}, blank_opts)
-		end
+		wk.register({
+			x = {
+				name = "Trouble", -- optional group name
+				x = { "<Cmd>TroubleToggle<CR>", "Toggle Trouble" },
+				w = { "<Cmd>TroubleToggle workspace_diagnostics<CR>", "Workspace Diagnostics" },
+				d = { "<Cmd>TroubleToggle document_diagnostics<CR>", "Document Diagnostics" },
+				q = { "<Cmd>TroubleToggle quickfix<CR>", "Quickfix" },
+			},
+		}, leader_opts)
+		wk.register({
+			gR = { "<Cmd>TroubleToggle lsp_references<CR>", "LSP References" },
+		}, blank_opts)
 
 		-- neogen --
-		local status_neogen, _ = pcall(require, "neogen")
-		if status_neogen then
-			wk.register({
-				d = { "<Cmd>Neogen<CR>", "Generate documentation" },
-			}, leader_opts)
-		end
+		wk.register({
+			d = { "<Cmd>Neogen<CR>", "Generate documentation" },
+		}, leader_opts)
 
 		-- Hop manual keymap register --
-		local status_hop, _ = pcall(require, "hop")
-		if status_hop then
-			wk.register({
-				f = { "<Cmd>HopChar1CurrentLineAC<CR>", "Hop Current Line After" },
-				F = { "<Cmd>HopChar1CurrentLineBC<CR>", "Hop Current Line Before" },
-			}, blank_opts)
-			wk.register({
-				h = { "<Cmd>HopChar1AC<CR>", "Hop After" },
-				H = { "<Cmd>HopChar1BC<CR>", "Hop Before" },
-			}, leader_opts)
-			wk.register({
-				h = { "<Cmd>HopChar2MW<CR>", "Hop Anywhere" },
-			}, leader2_opts)
-		end
+		wk.register({
+			f = { "<Cmd>HopChar1CurrentLineAC<CR>", "Hop Current Line After" },
+			F = { "<Cmd>HopChar1CurrentLineBC<CR>", "Hop Current Line Before" },
+		}, blank_opts)
+		wk.register({
+			h = { "<Cmd>HopChar1AC<CR>", "Hop After" },
+			H = { "<Cmd>HopChar1BC<CR>", "Hop Before" },
+		}, leader_opts)
+		wk.register({
+			h = { "<Cmd>HopChar2MW<CR>", "Hop Anywhere" },
+		}, leader2_opts)
 
 		-- LSP --
-		local status_lsp_toggle, _ = pcall(require, "lsp-toggle")
-		local status_toggle_lsp_diagnostics, _ = pcall(require, "toggle_lsp_diagnostics")
-		local status_lspsaga, _ = pcall(require, "lspsaga")
+		wk.register({
+			l = {
+				name = "LSP",
+				v = { "<Plug>(toggle-lsp-diag-vtext)", "Toggle LSP Virtual Text" },
+			},
+		}, leader_opts)
 
-		if status_toggle_lsp_diagnostics then
-			wk.register({
-				l = {
-					name = "LSP",
-					v = { "<Plug>(toggle-lsp-diag-vtext)", "Toggle LSP Virtual Text" },
-				},
-			}, leader_opts)
-		end
+		wk.register({
+			l = {
+				name = "LSP",
+				l = { "<Cmd>ToggleLSP<CR>", "ToggleLSP" },
+				n = { "<Cmd>ToggleNullLSP<CR>", "ToggleNullLSP" },
+			},
+		}, leader_opts)
 
-		if status_lsp_toggle then
-			wk.register({
-				l = {
-					name = "LSP",
-					l = { "<Cmd>ToggleLSP<CR>", "ToggleLSP" },
-					n = { "<Cmd>ToggleNullLSP<CR>", "ToggleNullLSP" },
-				},
-			}, leader_opts)
-		end
-
-		if status_lspsaga then
-			wk.register({
-				l = {
-					name = "LSP",
-					f = { "<Cmd>Lspsaga lsp_finder<CR>", "Lspsaga Definition Finder" },
-					o = { "<Cmd>Lspsaga outline<CR>", "Lspsaga Outline" },
-					c = { "<Cmd>Lspsaga code_action<CR>", "Lspsaga Code Action" },
-					d = { "<Cmd>Lspsaga show_line_diagnostics<CR>", "Lspsaga Show Line Diagnostics" },
-				},
-			}, leader_opts)
-		end
+		wk.register({
+			l = {
+				name = "LSP",
+				f = { "<Cmd>Lspsaga lsp_finder<CR>", "Lspsaga Definition Finder" },
+				o = { "<Cmd>Lspsaga outline<CR>", "Lspsaga Outline" },
+				c = { "<Cmd>Lspsaga code_action<CR>", "Lspsaga Code Action" },
+				d = { "<Cmd>Lspsaga show_line_diagnostics<CR>", "Lspsaga Show Line Diagnostics" },
+			},
+		}, leader_opts)
 
 		-- Null-ls --
-
-		local status_null_ls, _ = pcall(require, "null-ls")
-		if status_null_ls then
-			wk.register({
-				["="] = { "<Cmd>NullFormat<CR>", "Format Using Null-ls Only" },
-			}, leader_opts)
-		end
+		wk.register({
+			["="] = { "<Cmd>NullFormat<CR>", "Format Using Null-ls Only" },
+		}, leader_opts)
 
 		-- Default vim keymaps --
 		wk.register({
