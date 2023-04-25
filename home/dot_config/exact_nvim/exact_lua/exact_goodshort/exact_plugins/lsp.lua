26f9d15ad1bbe9ea626a1cf1c970e322058745e9
Adrien Biencourt <adrien@revanista.com>
2023-04-25 20:10:04 +0100

    Removed unecessary lines

--- home/dot_config/exact_nvim/exact_lua/exact_goodshort/exact_plugins/lsp.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_goodshort/exact_plugins/lsp.lua
@@ -114,54 +114,7 @@ return {
 				},
 			})
 
-			-- local on_attach = function(client, bufnr)
-			-- 	local opts = { buffer = bufnr, remap = false }
-			--
-			-- 	vim.keymap.set("n", "gd", function()
-			-- 		vim.lsp.buf.definition()
-			-- 	end, opts)
-			-- 	vim.keymap.set("n", "K", function()
-			-- 		vim.lsp.buf.hover()
-			-- 	end, opts)
-			-- 	vim.keymap.set("n", "<leader>e", function()
-			-- 		vim.diagnostic.open_float()
-			-- 	end, opts)
-			-- 	vim.keymap.set("n", "[d", function()
-			-- 		vim.diagnostic.goto_next()
-			-- 	end, opts)
-			-- 	vim.keymap.set("n", "]d", function()
-			-- 		vim.diagnostic.goto_prev()
-			-- 	end, opts)
-			-- 	vim.keymap.set("n", "<leader>ca", function()
-			-- 		vim.lsp.buf.code_action()
-			-- 	end, opts)
-			-- 	vim.keymap.set("n", "<leader>gr", function()
-			-- 		vim.lsp.buf.references()
-			-- 	end, opts)
-			-- 	vim.keymap.set("n", "<leader>rn", function()
-			-- 		vim.lsp.buf.rename()
-			-- 	end, opts)
-			-- 	vim.keymap.set("i", "<C-h>", function()
-			-- 		vim.lsp.buf.signature_help()
-			-- 	end, opts)
-			-- 	vim.keymap.set("n", "<leader>dl", function()
-			-- 		vim.diagnostic.setqflist()
-			-- 	end, opts)
-			-- end
-			--
-			-- lsp.on_attach(on_attach)
-
 			lsp.setup()
-
-			-- Restore built-in config for diagnostic
-			vim.diagnostic.config({
-				virtual_text = true,
-				signs = true,
-				update_in_insert = false,
-				underline = true,
-				severity_sort = false,
-				float = true,
-			})
 
 			-- null-ls
 			local null_ls = require("null-ls")
