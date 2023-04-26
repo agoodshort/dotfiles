c04c213d79764c7a44cf3dc5539985137af5a2fc
Adrien Biencourt <adrien@revanista.com>
2023-04-26 22:02:17 +0100

    Toggle toggleterm off with unception

--- home/dot_config/exact_nvim/exact_lua/exact_goodshort/exact_plugins/init.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_goodshort/exact_plugins/init.lua
@@ -36,8 +36,16 @@ return {
 
 	{
 		"samjwill/nvim-unception",
-		config = function()
+		init = function()
 			vim.g.unception_open_buffer_in_new_tab = true
+
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "UnceptionEditRequestReceived",
+				callback = function()
+					-- Toggle the terminal off.
+					vim.cmd("9ToggleTerm")
+				end,
+			})
 		end,
 	},
 }
