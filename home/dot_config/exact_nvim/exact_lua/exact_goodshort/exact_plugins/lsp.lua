19b08148e153d0bf8fc0f1ef6a29792e2efd9be5
Adrien Biencourt <adrien@revanista.com>
2023-04-27 17:48:33 +0100

    Moved toggle lsp out

--- home/dot_config/exact_nvim/exact_lua/exact_goodshort/exact_plugins/lsp.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_goodshort/exact_plugins/lsp.lua
@@ -1,89 +1,100 @@
 return {
-	"jay-babu/mason-null-ls.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"williamboman/mason.nvim",
-		"jose-elias-alvarez/null-ls.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-		"hrsh7th/cmp-nvim-lsp",
-		"folke/neodev.nvim",
-		{
-			"WhoIsSethDaniel/toggle-lsp-diagnostics.nvim",
-			config = function()
-				require("toggle_lsp_diagnostics").init()
-			end,
-		},
+	{
+		"WhoIsSethDaniel/toggle-lsp-diagnostics.nvim",
+		event = "LspAttach",
+		config = function()
+			require("toggle_lsp_diagnostics").init()
+		end,
 	},
-	config = function()
-		require("mason").setup()
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"jose-elias-alvarez/null-ls.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+			"folke/neodev.nvim",
+			"kevinhwang91/nvim-ufo",
+		},
+		config = function()
+			require("mason").setup()
 
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"jsonls",
-				"lua_ls",
-				"tsserver",
-				"powershell_es",
-				"yamlls",
-				"marksman",
-			},
-		})
-
-		local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-		-- local lsp_attach = function(client, bufnr)
-		-- 	-- Create your keybindings here...
-		-- end
-		--
-		require("neodev").setup({})
-
-		local lspconfig = require("lspconfig")
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				lspconfig[server_name].setup({
-					-- on_attach = lsp_attach,
-					capabilities = lsp_capabilities,
-				})
-			end,
-		})
-
-		lspconfig.yamlls.setup({
-			settings = {
-				yaml = {
-					keyOrdering = false,
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"jsonls",
+					"lua_ls",
+					"tsserver",
+					"powershell_es",
+					"yamlls",
+					"marksman",
 				},
-			},
-		})
-		require("mason-null-ls").setup({
-			ensure_installed = { "stylua", "prettier", "beautysh", "black", "cfn_lint" },
-			automatic_installation = true,
-			automatic_setup = true,
-		})
+			})
 
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			-- Creates function `NullFormat` to format only using null_ls
-			on_attach = function(client, bufnr)
-				local format_cmd = function(input)
-					vim.lsp.buf.format({
-						id = client.id,
-						timeout_ms = 5000,
-						async = input.bang,
+			local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			-- Add folding capabilities required by ufo.nvim
+			lsp_capabilities.textDocument.foldingRange = {
+				dynamicRegistration = false,
+				lineFoldingOnly = true,
+			}
+
+			-- local lsp_attach = function(client, bufnr)
+			-- 	-- Create your keybindings here...
+			-- end
+			--
+			require("neodev").setup({})
+
+			local lspconfig = require("lspconfig")
+			require("mason-lspconfig").setup_handlers({
+				function(server_name)
+					lspconfig[server_name].setup({
+						-- on_attach = lsp_attach,
+						capabilities = lsp_capabilities,
 					})
-				end
-				local bufcmd = vim.api.nvim_buf_create_user_command
-				bufcmd(bufnr, "NullFormat", format_cmd, {
-					bang = true,
-					range = true,
-					desc = "Format Using Null-ls Only",
-				})
-			end,
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.beautysh,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.diagnostics.cfn_lint,
-			},
-		})
-	end,
+				end,
+			})
+
+			lspconfig.yamlls.setup({
+				settings = {
+					yaml = {
+						keyOrdering = false,
+					},
+				},
+			})
+			require("mason-null-ls").setup({
+				ensure_installed = { "stylua", "prettier", "beautysh", "black", "cfn_lint" },
+				automatic_installation = true,
+				automatic_setup = true,
+			})
+
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				-- Creates function `NullFormat` to format only using null_ls
+				on_attach = function(client, bufnr)
+					local format_cmd = function(input)
+						vim.lsp.buf.format({
+							id = client.id,
+							timeout_ms = 5000,
+							async = input.bang,
+						})
+					end
+					local bufcmd = vim.api.nvim_buf_create_user_command
+					bufcmd(bufnr, "NullFormat", format_cmd, {
+						bang = true,
+						range = true,
+						desc = "Format Using Null-ls Only",
+					})
+				end,
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.formatting.beautysh,
+					null_ls.builtins.formatting.black,
+					null_ls.builtins.diagnostics.cfn_lint,
+				},
+			})
+		end,
+	},
 }
