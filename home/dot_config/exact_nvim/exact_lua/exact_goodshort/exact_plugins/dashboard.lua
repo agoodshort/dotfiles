cec4d888b51ad877bd8565ad5f2eef48e7900d25
Adrien Biencourt <adrien@revanista.com>
2023-05-06 22:31:48 +0100

    Using opts instead of config and plugin.setup()

--- home/dot_config/exact_nvim/exact_lua/exact_goodshort/exact_plugins/dashboard.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_goodshort/exact_plugins/dashboard.lua
@@ -2,47 +2,45 @@ return {
 	"glepnir/dashboard-nvim",
 	event = "VimEnter",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("dashboard").setup({
-			theme = "hyper",
-			hide = {
-				statusline = true, -- hide statusline default is true
-				tabline = false, -- hide the tabline
-				winbar = true, -- hide winbar
+	opts = {
+		theme = "hyper",
+		hide = {
+			statusline = true, -- hide statusline default is true
+			tabline = false, -- hide the tabline
+			winbar = true, -- hide winbar
+		},
+		config = {
+			week_header = {
+				enable = true,
 			},
-			config = {
-				week_header = {
-					enable = true,
+			packages = { enable = true }, -- show how many plugins neovim loaded
+			project = { limit = 8, action = "edit" },
+			shortcut = {
+				{
+					desc = " Lazy",
+					group = "@property",
+					action = "Lazy",
+					key = "l",
 				},
-				packages = { enable = true }, -- show how many plugins neovim loaded
-				project = { limit = 8, action = "edit" },
-				shortcut = {
-					{
-						desc = " Lazy",
-						group = "@property",
-						action = "Lazy",
-						key = "l",
-					},
-					{
-						desc = " Files",
-						group = "Label",
-						action = "Neotree position=current",
-						key = "f",
-					},
-					{
-						desc = " Dotfiles",
-						group = "DiagnosticHint",
-						action = "edit $XDG_CONFIG_HOME",
-						key = "d",
-					},
-					{
-						desc = " Configuration",
-						group = "Number",
-						action = "edit $XDG_CONFIG_HOME/nvim",
-						key = "c",
-					},
+				{
+					desc = " Files",
+					group = "Label",
+					action = "Neotree position=current",
+					key = "f",
+				},
+				{
+					desc = " Dotfiles",
+					group = "DiagnosticHint",
+					action = "edit $XDG_CONFIG_HOME",
+					key = "d",
+				},
+				{
+					desc = " Configuration",
+					group = "Number",
+					action = "edit $XDG_CONFIG_HOME/nvim",
+					key = "c",
 				},
 			},
-		})
-	end,
+		},
+	},
 }
