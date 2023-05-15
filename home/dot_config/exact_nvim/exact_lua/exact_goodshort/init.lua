d63fc1dd488db8e1ef54fc53d85033b11dcaf1b5
Adrien Biencourt <adrien@revanista.com>
2023-05-15 13:45:19 +0100

    Added VSCode extension

--- home/dot_config/exact_nvim/exact_lua/exact_goodshort/init.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_goodshort/init.lua
@@ -1,25 +1,30 @@
 require("goodshort.set")
 require("goodshort.remap")
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
+if vim.g.vscode then
+	-- VSCode extension
+else
+	-- ordinary Neovim
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not vim.loop.fs_stat(lazypath) then
+		vim.fn.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/folke/lazy.nvim.git",
+			"--branch=stable", -- latest stable release
+			lazypath,
+		})
+	end
+	vim.opt.rtp:prepend(lazypath)
 
--- import lazy safely
-local status, lazy = pcall(require, "lazy")
-if not status then
-	return
-end
+	-- import lazy safely
+	local status, lazy = pcall(require, "lazy")
+	if not status then
+		return
+	end
 
-lazy.setup("goodshort.plugins", { defaults = {
-	lazy = false,
-} })
+	lazy.setup("goodshort.plugins", { defaults = {
+		lazy = false,
+	} })
+end
