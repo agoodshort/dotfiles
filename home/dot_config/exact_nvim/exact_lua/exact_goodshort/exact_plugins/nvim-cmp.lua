9cb4f435bfc6d6207a01b581eb619d9d8df2849e
Adrien Biencourt <adrien@revanista.com>
2023-05-15 13:54:09 +0100

    Fixed autocomplete

--- home/dot_config/exact_nvim/exact_lua/exact_goodshort/exact_plugins/nvim-cmp.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_goodshort/exact_plugins/nvim-cmp.lua
@@ -1,81 +1,102 @@
 return {
-	-- Autocompletion
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		-- LuaSnip required
-		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
-		-- Recommended
-		"neovim/nvim-lspconfig",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/nvim-cmp",
-		-- My own
-		"hrsh7th/cmp-nvim-lua",
-		{
-			"danymat/neogen", -- write documentation
-			config = function()
-				require("neogen").setup({
-					enabled = true, --if you want to disable Neogen
-					snippet_engine = "luasnip",
-					input_after_comment = true, -- (default: true) automatic jump (with insert mode) on inserted annotation
-				})
-			end,
-			dependencies = { "nvim-treesitter/nvim-treesitter" },
-			version = "*", -- folllow only stable versions
-		},
+	{
+		"danymat/neogen", -- write documentation
+		lazy = true,
+		config = function()
+			require("neogen").setup({
+				enabled = true, --if you want to disable Neogen
+				snippet_engine = "luasnip",
+				input_after_comment = true, -- (default: true) automatic jump (with insert mode) on inserted annotation
+			})
+		end,
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		version = "*", -- folllow only stable versions
+	},
+	{
 		"David-Kunz/cmp-npm",
+		lazy = true,
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 		},
-		opts = {},
+		opts = {
+			ignore = {},
+			only_semantic_versions = false,
+			only_latest_version = false,
+		},
 	},
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
+	{
+		"hrsh7th/nvim-cmp", -- Autocompletion
+		event = "InsertEnter",
+		dependencies = {
+			-- LuaSnip required
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			-- Recommended
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/nvim-cmp",
+			-- My own
+			"hrsh7th/cmp-nvim-lua",
+			"rafamadriz/friendly-snippets",
+			"danymat/neogen", -- Configured above
+			"David-Kunz/cmp-npm", -- Configured above
+			"onsails/lspkind.nvim",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			local lspkind = require("lspkind")
 
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body)
-				end,
-			},
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "path" },
-				{ name = "buffer" },
-				{ name = "nvim_lsp" },
-				{ name = "nvim_lua" },
-				{ name = "npm", keyword_length = 4 },
-			},
-			mapping = {
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				["<C-j>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-					-- they way you will only jump inside the snippet region
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<C-k>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-			},
-		})
-	end,
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "path" },
+					{ name = "buffer" },
+					{ name = "nvim_lsp" },
+					{ name = "nvim_lua" },
+					{ name = "npm", keyword_length = 4 },
+				},
+				mapping = {
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<C-j>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<C-k>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				},
+				formatting = {
+					format = lspkind.cmp_format({
+						mode = "symbol_text",
+						maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+						ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+					}),
+				},
+			})
+
+			require("luasnip.loaders.from_vscode").lazy_load() -- Load friendly-snippets
+		end,
+	},
 }
