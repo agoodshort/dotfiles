b55ee6c5bd61ee10603da371100c9622e4710e63
agoodshort <adrien.goodshort@gmail.com>
2023-05-16 23:14:31 +0100

    Moved trick directly in plugin config

--- home/dot_config/exact_nvim/exact_lua/exact_goodshort/exact_plugins/init.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_goodshort/exact_plugins/init.lua
@@ -29,6 +29,7 @@ return {
 		"samjwill/nvim-unception",
 		init = function()
 			vim.g.unception_open_buffer_in_new_tab = true
+			vim.cmd([[autocmd FileType gitcommit,gitrebase,gitconfig set bufhidden=delete]])
 
 			vim.api.nvim_create_autocmd("User", {
 				pattern = "UnceptionEditRequestReceived",
--- home/dot_config/exact_nvim/exact_lua/exact_goodshort/set.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_goodshort/set.lua
@@ -28,9 +28,6 @@ vim.opt.termguicolors = true
 -- Vim updates faster
 vim.opt.updatetime = 250
 
--- nvim-unception trick
-vim.cmd([[autocmd FileType gitcommit,gitrebase,gitconfig set bufhidden=delete]])
-
 -- Make sure .env.* files get syntax hightlight
 vim.cmd([[autocmd BufNewFile,BufReadPost .env.* set ft=sh]])
 vim.cmd([[autocmd BufNewFile,BufReadPost Brewfile* set ft=sh]])
