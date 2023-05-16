bbf8d19e0429b0a0a8b8c29fc1c258b027489ded
agoodshort <adrien.goodshort@gmail.com>
2023-05-16 23:17:56 +0100

    Testing configs with lualine

--- home/dot_config/exact_nvim/exact_lua/exact_goodshort/exact_plugins/lualine.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_goodshort/exact_plugins/lualine.lua
@@ -1,12 +1,17 @@
 return {
 	{
 		"b0o/incline.nvim",
-		dependencies = "goodshort/staline.nvim",
+		dependencies = "nvim-lualine/lualine.nvim",
 		opts = {},
 	},
 	{
 		"goodshort/staline.nvim",
+		enabled = false,
 		branch = "fn_color_invert",
+		dependencies = {
+			"kanagawa.nvim",
+			{ "someone-stole-my-name/yaml-companion.nvim", dependencies = "neovim/nvim-lspconfig" },
+		},
 		config = function()
 			-- Sets the statusline to full width
 			vim.opt.laststatus = 3
@@ -15,8 +20,35 @@ return {
 				return vim.fn.expand("%:p:.")
 			end
 
+			local function codeium()
+				return vim.fn["codeium#GetStatusString"]()
+			end
+
+			local function yaml_companion()
+				local schema = require("yaml-companion").get_buf_schema(0)
+				if schema.result[1].name == "none" then
+					return " "
+				end
+				return schema.result[1].name
+			end
+
+			local function lsp_name()
+				local msg = "No Active Lsp"
+				local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+				local clients = vim.lsp.get_active_clients()
+				if next(clients) == nil then
+					return msg
+				end
+				for _, client in ipairs(clients) do
+					local filetypes = client.config.filetypes
+					if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+						return client.name
+					end
+				end
+				return msg
+			end
+
 			require("staline").setup({
-				-- need to add yaml-companion
 				sections = {
 					left = {
 						" ",
@@ -25,6 +57,7 @@ return {
 						"left_sep",
 						" ",
 						"right_sep",
+						"-cool_symbol",
 						fileloc,
 						"left_sep",
 						" ",
@@ -35,14 +68,21 @@ return {
 					},
 					mid = { "lsp" },
 					right = {
+						" ",
 						"right_sep",
-						"-cool_symbol",
+						"- ",
+						lsp_name,
+						"- ",
+						"-lsp",
 						"left_sep",
 						" ",
 						"right_sep",
-						"- ",
-						"-lsp_name",
-						"- ",
+						yaml_companion,
+						"left_sep",
+						" ",
+						"right_sep",
+						"- ",
+						codeium,
 						"left_sep",
 						" ",
 					},
@@ -59,7 +99,7 @@ return {
 	},
 	{
 		"nvim-lualine/lualine.nvim",
-		enabled = false,
+		-- enabled = false,
 		dependencies = {
 			"kanagawa.nvim",
 			{ "someone-stole-my-name/yaml-companion.nvim", dependencies = "neovim/nvim-lspconfig" },
@@ -73,44 +113,14 @@ return {
 				return vim.fn["codeium#GetStatusString"]()
 			end
 
-			local colors = {
-				blue = "#80a0ff",
-				cyan = "#79dac8",
-				black = "#080808",
-				white = "#c6c6c6",
-				red = "#ff5189",
-				violet = "#d183e8",
-				grey = "#303030",
-			}
-
-			local bubbles_theme = {
-				normal = {
-					a = { fg = colors.black, bg = colors.violet },
-					b = { fg = colors.white, bg = colors.grey },
-					c = { fg = colors.black, bg = colors.black },
-				},
-
-				insert = { a = { fg = colors.black, bg = colors.blue } },
-				visual = { a = { fg = colors.black, bg = colors.cyan } },
-				replace = { a = { fg = colors.black, bg = colors.red } },
-
-				inactive = {
-					a = { fg = colors.white, bg = colors.black },
-					b = { fg = colors.white, bg = colors.black },
-					c = { fg = colors.black, bg = colors.black },
-				},
-			}
-
 			local config = {
 				options = {
-					theme = bubbles_theme,
-					disabled_filetypes = { "packer", "neo-tree", "lspsagaoutline" },
-					component_separators = "|",
-					-- section_separators = { left = " ", right = " " },
+					disabled_filetypes = { "dashboard", "neo-tree", "lspsagaoutline" },
+					-- component_separators = "|",
 				},
 				sections = {
-					lualine_a = { { "mode", separator = { left = "", right = "" } } },
-					lualine_b = { { fileloc, separator = { left = "", right = "" } } },
+					lualine_a = { "mode" },
+					lualine_b = { fileloc },
 					lualine_c = { "branch", "diff" },
 					lualine_x = { "diagnostics" },
 					lualine_y = { "filetype" },
@@ -118,8 +128,6 @@ return {
 						{
 							codeium,
 							icon = "",
-							separator = { right = "" },
-							left_padding = 2,
 						},
 					},
 				},
@@ -144,7 +152,7 @@ return {
 			-- Lsp server name
 			ins_left({
 				function()
-					local msg = vim.cmd("call codeium#GetStatusString()")
+					local msg = "No Active Lsp"
 					local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
 					local clients = vim.lsp.get_active_clients()
 					if next(clients) == nil then
@@ -158,18 +166,16 @@ return {
 					end
 					return msg
 				end,
-				icon = { "", color = { fg = "green", bg = "black" } },
+				icon = "",
 			})
-			-- yaml compagnion
-			ins_left({
-				function()
-					local schema = require("yaml-companion").get_buf_schema(0)
-					if schema.result[1].name == "none" then
-						return ""
-					end
-					return schema.result[1].name
-				end,
-			})
+
+			-- yaml-companion
+			local status, schema = pcall(require("yaml-companion").get_buf_schema, 0)
+			if status then
+				if schema.result[1].name ~= "none" then
+					ins_left(schema.result[1].name)
+				end
+			end
 
 			require("lualine").setup(config)
 		end,
