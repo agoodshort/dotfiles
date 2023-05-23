f0cfd0bc247815a4f0a95c562903465fcd1597dd
agoodshort <adrien.goodshort@gmail.com>
2023-05-23 23:44:01 +0100

    Added Markdown specific plugins

--- home/dot_config/exact_nvim/exact_lua/exact_agoodshort/exact_plugins/nvim-treesitter.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_agoodshort/exact_plugins/nvim-treesitter.lua
@@ -1,56 +1,72 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = function()
-		require("nvim-treesitter.install").update({ with_sync = true })
-	end,
-	event = "BufReadPost",
-	dependencies = { "HiPhish/nvim-ts-rainbow2", "windwp/nvim-ts-autotag" },
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			highlight = {
-				enable = true,
+	{
+		"AckslD/nvim-FeMaco.lua", -- Edit code blocks in Markdown in a separate window
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		opts = {},
+	},
+	{
+		"lukas-reineke/headlines.nvim",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		opts = {
+			markdown = {
+				fat_headline_lower_string = "▔",
 			},
-			autotag = {
-				enable = true,
-			},
-			indent = { enable = true },
-			ensure_installed = {
-				"json",
-				"javascript",
-				"typescript",
-				"tsx",
-				"yaml",
-				"html",
-				"css",
-				"markdown",
-				"markdown_inline",
-				"bash",
-				"lua",
-				"vim",
-				"dockerfile",
-				"gitignore",
-				"regex",
-			},
-			-- select blocks with <CR>
-			auto_install = true,
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<CR>",
-					node_incremental = "<CR>",
-					scope_incremental = "<S-CR>",
-					node_decremental = "<BS>",
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+		event = "BufReadPost",
+		dependencies = { "HiPhish/nvim-ts-rainbow2", "windwp/nvim-ts-autotag" },
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				highlight = {
+					enable = true,
 				},
-			},
-			-- nvim-ts-rainbow
-			rainbow = {
-				enable = true,
-				-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-				-- Which query to use for finding delimiters
-				query = "rainbow-parens",
-				-- Highlight the entire buffer all at once
-				strategy = require("ts-rainbow").strategy.global,
-			},
-		})
-	end,
+				autotag = {
+					enable = true,
+				},
+				indent = { enable = true },
+				ensure_installed = {
+					"json",
+					"javascript",
+					"typescript",
+					"tsx",
+					"yaml",
+					"html",
+					"css",
+					"markdown",
+					"markdown_inline",
+					"bash",
+					"lua",
+					"vim",
+					"dockerfile",
+					"gitignore",
+					"regex",
+				},
+				-- select blocks with <CR>
+				auto_install = true,
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<CR>",
+						node_incremental = "<CR>",
+						scope_incremental = "<S-CR>",
+						node_decremental = "<BS>",
+					},
+				},
+				-- nvim-ts-rainbow
+				rainbow = {
+					enable = true,
+					-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+					-- Which query to use for finding delimiters
+					query = "rainbow-parens",
+					-- Highlight the entire buffer all at once
+					strategy = require("ts-rainbow").strategy.global,
+				},
+			})
+		end,
+	},
 }
