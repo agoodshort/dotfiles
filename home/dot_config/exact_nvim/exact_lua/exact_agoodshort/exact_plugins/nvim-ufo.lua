fbf6c401520d9637f1b4e0ba10edb4130ac4c05e
agoodshort <adrien.goodshort@gmail.com>
2023-06-05 03:15:14 +0100

    Fixed nvim-ufo

--- home/dot_config/exact_nvim/exact_lua/exact_agoodshort/exact_plugins/nvim-ufo.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_agoodshort/exact_plugins/nvim-ufo.lua
@@ -1,16 +1,16 @@
 return {
 	{
 		"kevinhwang91/nvim-ufo",
-		enabled = false,
-		lazy = true,
-		dependencies = "kevinhwang91/promise-async",
-		config = function()
+		event = "BufReadPost",
+		dependencies = { "kevinhwang91/promise-async", "luukvbaal/statuscol.nvim" },
+		init = function()
 			vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-			vim.o.foldcolumn = "auto" -- '0' is not bad
 			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 			vim.o.foldlevelstart = 99
 			vim.o.foldenable = true
-
+			vim.wo.foldcolumn = "1"
+		end,
+		config = function()
 			-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 			vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 			vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
@@ -20,7 +20,6 @@ return {
 				vim = "indent",
 				python = { "indent" },
 				git = "",
-				["neo-tree"] = "",
 			}
 
 			local function customizeSelector(bufnr)
@@ -46,6 +45,24 @@ return {
 				provider_selector = function(bufnr, filetype, buftype)
 					return ftMap[filetype] or customizeSelector
 				end,
+			})
+		end,
+	},
+	{
+		"luukvbaal/statuscol.nvim",
+		lazy = true,
+		config = function()
+			local builtin = require("statuscol.builtin")
+			require("statuscol").setup({
+				segments = {
+					{ text = { "%s" }, click = "v:lua.ScSa" },
+					{ text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+					{
+						text = { " ", builtin.foldfunc, " " },
+						condition = { builtin.not_empty, true, builtin.not_empty },
+						click = "v:lua.ScFa",
+					},
+				},
 			})
 		end,
 	},
