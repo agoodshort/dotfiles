b2fb651736e832802dd5b4b08df57147842c52d6
agoodshort <adrien.goodshort@gmail.com>
2023-07-21 09:56:39 +0100

    Fixed indent lines in dashboard

--- home/dot_config/exact_nvim/exact_lua/exact_agoodshort/exact_plugins/indent-blankline.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_agoodshort/exact_plugins/indent-blankline.lua
@@ -4,6 +4,15 @@ return {
 	config = function()
 		vim.opt.list = true
 		vim.opt.listchars:append("lead:⋅")
+		vim.g.indent_blankline_filetype_exclude = {
+			"lspinfo",
+			"packer",
+			"checkhealth",
+			"help",
+			"man",
+			"",
+			"dashboard",
+		}
 
 		vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
 		vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
