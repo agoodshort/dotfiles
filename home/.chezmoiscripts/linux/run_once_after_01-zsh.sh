a319c9320e48e39bb4777181d5ca65d7046fd008
agoodshort <adrien.goodshort@gmail.com>
2024-01-06 22:23:05 +0000

    refactor(scripts): use variable to display warnings

--- home/.chezmoiscripts/linux/run_once_after_01-zsh.sh
+++ home/.chezmoiscripts/linux/run_once_after_01-zsh.sh
@@ -7,8 +7,8 @@ if [[ $SHELL != *"zsh"* ]]; then
         sudo sh -c "echo $(which zsh) >> /etc/shells"
     fi
     chsh -s "$(which zsh)"
-    echo "/!\ Default shell changed to zsh /!\ "
-    echo "/!\ Log out and log back in to use zsh /!\ "
+    chezmoi_warnings+=("/!\ Default shell changed to zsh /!\ ")
+    chezmoi_warnings+=("/!\ Log out and log back in to use zsh /!\ ")
 else
     echo "zsh is already the default shell"
 fi
