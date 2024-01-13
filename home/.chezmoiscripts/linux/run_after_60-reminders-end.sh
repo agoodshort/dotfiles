fc4af8120f0d874e1661fbd1089a4532b42f3c77
agoodshort <adrien.goodshort@gmail.com>
2024-01-13 15:28:26 +0000

    fix(scripts): check if chezmoi_warnings is defined

--- home/.chezmoiscripts/linux/run_after_60-reminders-end.sh
+++ home/.chezmoiscripts/linux/run_after_60-reminders-end.sh
@@ -2,12 +2,12 @@
 
 echo "----- start: 60-reminder-end.sh -----"
 
-if [[-v chezmoi_warnings ]]; then
-    for chezmoi_warning in "${chezmoi_warnings[@]}"; do
-        echo "$chezmoi_warning"
-    done
+if declare -p chezmoi_warnings &> /dev/null; then
+  for chezmoi_warning in "${chezmoi_warnings[@]}"; do
+    echo "$chezmoi_warning"
+  done
 else
-    echo "No chezmoi warnings to display"
+  echo "No chezmoi warnings to display"
 fi
 
 echo "----- end: 60-reminder-end.sh -----"
