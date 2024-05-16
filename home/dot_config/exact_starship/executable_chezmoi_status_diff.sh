28ac16d16c5a6705c5f6d7aeab9bc6b2d8406b65
agoodshort <adrien.goodshort@gmail.com>
2024-05-16 09:30:45 +0100

    feat(chezmoi): exclude scripts on status

--- home/dot_config/exact_starship/executable_chezmoi_status_diff.sh
+++ home/dot_config/exact_starship/executable_chezmoi_status_diff.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 output=""
-status=$(chezmoi status 2>&1)
+status=$(chezmoi status -x scripts 2>&1)
 
-[[ "$status" == *"warning:"*  ]] && output="${output}" # Unlikely that a file is called "warning:"
+[[ "$status" == *"warning:"* ]] && output="${output}" # Unlikely that a file is called "warning:"
 
 sourceStatus=$(echo "$status" | cut -c 2)
 destinationStatus=$(echo "$status" | cut -c 1)
 
 # destination
-[[ $(echo $destinationStatus | wc -l) > 0 ]] && output="${output}⇡"
+[[ $(echo "$destinationStatus" | wc -l) -gt 0 ]] && output="${output}⇡"
 
 # source
-[[ $(echo $sourceStatus | wc -l) > 0 ]] && output="${output}⇣"
+[[ $(echo "$sourceStatus" | wc -l) -gt 0 ]] && output="${output}⇣"
 
 # modified
 [[ ($sourceStatus == *'M'*) || ($destinationStatus == *'M'*) ]] && output="${output}!"
